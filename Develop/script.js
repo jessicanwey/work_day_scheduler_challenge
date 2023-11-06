1c1e6d6e9bbacb8dd1f40c3f88292d804f1bb596
jessicanwey <145372607+jessicanwey@users.noreply.github.com>
2023-11-06 15:39:19 -0500

    all functionality is working

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
 var today = dayjs();
 var currentHour = today.hour();
@@ -21,51 +18,40 @@ $(function () {
     hourId = $(this).parent().attr("id");
     console.log(hourId);
 
-    localStorage.setItem(activity, hourId);
+    localStorage.setItem(hourId, activity);
   });
 
   //loop through all of the time block divs an apply appropriate styling based on the current time vs the time block on the calendar
-  setHourBlockFormat();
+    $(timeBlockElement).each(function(){
+      var timeBlockId = parseInt($(this).attr('id').split("hour-")[1]);
+      console.log(timeBlockId);
+  
+      if(timeBlockId < currentHour){
+        $(this).addClass('past');
+      } else if (timeBlockId === currentHour){
+        $(this).removeClass('past');
+        $(this).removeClass('future');
+        $(this).addClass('present');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+  
+    })
+   
 
-  //get the values of the activities that were saved to each time block if there are any
-  getTimeBlockActivity();
-
-  console.log('current hour ' + currentHour);
+  //get the values of the activities that were saved to each time block in the 
+  //local storage and display them in the correct time block
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 });
 
-function setHourBlockFormat(){
 
-  $(timeBlockElement).each(function(){
-    var timeBlockId = parseInt($(this).attr('id').split("hour-")[1]);
-    console.log(timeBlockId);
-
-    if(timeBlockId < currentHour){
-      $(this).addClass('past');
-    } else if (timeBlockId === currentHour){
-      $(this).removeClass('past');
-      $(this).removeClass('future');
-      $(this).addClass('present');
-    } else {
-      $(this).removeClass('past');
-      $(this).removeClass('present');
-      $(this).addClass('future');
-    }
-
-  })
-};
-
-function getTimeBlockActivity(){
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  console.log($("#hour-9 .description"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-};
